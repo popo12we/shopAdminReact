124c723c7daf4d2d328842acb99cfa5389bbc66b
lujunhui <385935631@qq.com>
2020-02-10 11:04:30 +0800

    登录初步

--- src/login/index.js
+++ src/login/index.js
@@ -1,32 +1,97 @@
 import React from 'react'
 import { Form, Input, Button } from 'element-react'
 import './index.css'
-export default class Login extends React.Component {
-  state = {
-    form: {
-      username: '',
-      password: ''
+class Elform extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          }
+        ]
+      }
     }
   }
+  handleSubmit(e) {
+    e.preventDefault()
+
+    this.refs.form.validate(valid => {
+      if (valid) {
+        alert('submit!')
+      } else {
+        console.log('error submit!!')
+        return false
+      }
+    })
+  }
+
+  handleReset(e) {
+    e.preventDefault()
+
+    this.refs.form.resetFields()
+  }
+
+  onChange(key, value) {
+    this.setState({
+      form: Object.assign({}, this.state.form, { [key]: value })
+    })
+  }
+
+  render() {
+    return (
+      <Form
+        ref="form"
+        model={this.state.form}
+        rules={this.state.rules}
+        labelWidth="80"
+        className="demo-ruleForm"
+      >
+        <Form.Item label="用户名" prop="username">
+          <Input
+            value={this.state.form.username}
+            onChange={this.onChange.bind(this, 'username')}
+          ></Input>
+        </Form.Item>
+        <Form.Item label="密码" prop="password">
+          <Input
+            value={this.state.form.password}
+            onChange={this.onChange.bind(this, 'password')}
+          ></Input>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" onClick={this.handleSubmit.bind(this)}>
+            登录
+          </Button>
+          <Button onClick={this.handleReset.bind(this)} className="reset">
+            重置
+          </Button>
+        </Form.Item>
+      </Form>
+    )
+  }
+}
+export default class Login extends React.Component {
   render() {
     return (
       <div className="login">
         {/* 表单 */}
         <div className="form">
-          <Form model={this.state.form} labelWidth="80">
-            <Form.Item label="用户名">
-              <Input value={this.state.form.username}></Input>
-            </Form.Item>
-            <Form.Item label="密码">
-              <Input value={this.state.form.password}></Input>
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" nativeType="submit">
-                登录
-              </Button>
-              <Button className="reset">重置</Button>
-            </Form.Item>
-          </Form>
+          <Elform></Elform>
         </div>
       </div>
     )
