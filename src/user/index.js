bd815dbb8d6dd291fe1558a4f2967b96cf20e1bd
lujunhui <385935631@qq.com>
2020-02-11 13:41:30 +0800

    表格样式

--- src/user/index.js
+++ src/user/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Breadcrumb } from 'element-react'
+import { Input, Button, Table } from 'element-react'
 import './index.css'
 // 面包屑
 class BoardList extends React.Component {
@@ -12,11 +13,102 @@ class BoardList extends React.Component {
     )
   }
 }
+// 搜索框
+class SearchInput extends React.Component {
+  render() {
+    return (
+      <div className="searchInput">
+        <Input placeholder="请输入内容" />
+        <Button icon="search"></Button>
+      </div>
+    )
+  }
+}
+// 表格
+class ElTable extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      columns: [
+        {
+          label: '日期',
+          prop: 'date',
+          width: 180
+        },
+        {
+          label: '姓名',
+          prop: 'name',
+          width: 180
+        },
+        {
+          label: '地址',
+          prop: 'address'
+        }
+      ],
+      data: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <Table
+        style={{ width: '100%' }}
+        columns={this.state.columns}
+        maxHeight={200}
+        data={this.state.data}
+      />
+    )
+  }
+}
 export default class User extends React.Component {
   render() {
     return (
       <div className="user">
         <BoardList></BoardList>
+        <SearchInput></SearchInput>
+        <ElTable></ElTable>
       </div>
     )
   }
