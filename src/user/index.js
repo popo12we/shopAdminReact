a29483d8cde21bab3c4f1505ad40988a5a2b0d34
lujunhui <385935631@qq.com>
2020-02-11 14:15:09 +0800

    分页器样式

--- src/user/index.js
+++ src/user/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Breadcrumb } from 'element-react'
-import { Input, Button, Table } from 'element-react'
+import { Input, Button, Table, Icon, Tag, Pagination } from 'element-react'
 import './index.css'
 // 面包屑
 class BoardList extends React.Component {
@@ -32,60 +32,102 @@ class ElTable extends React.Component {
     this.state = {
       columns: [
         {
+          type: 'index'
+        },
+        {
           label: '日期',
           prop: 'date',
-          width: 180
+          width: 150,
+          render: function(data) {
+            return (
+              <span>
+                <Icon name="time" />
+                <span style={{ marginLeft: '10px' }}>{data.date}</span>
+              </span>
+            )
+          }
         },
         {
           label: '姓名',
           prop: 'name',
-          width: 180
+          width: 160,
+          render: function(data) {
+            return <Tag>{data.name}</Tag>
+          }
         },
         {
-          label: '地址',
-          prop: 'address'
+          label: '操作',
+          prop: 'address',
+          render: function() {
+            return (
+              <span>
+                <Button plain={true} type="info" size="small">
+                  编辑
+                </Button>
+                <Button type="danger" size="small">
+                  删除
+                </Button>
+              </span>
+            )
+          }
         }
       ],
       data: [
         {
           date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
         },
         {
           date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
         },
         {
-          date: '2016-05-04',
+          date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
         },
         {
-          date: '2016-05-01',
+          date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
         },
         {
-          date: '2016-05-03',
+          date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
         }
       ]
     }
@@ -96,9 +138,29 @@ class ElTable extends React.Component {
       <Table
         style={{ width: '100%' }}
         columns={this.state.columns}
-        maxHeight={200}
         data={this.state.data}
+        border={true}
+        highlightCurrentRow={true}
+        onCurrentChange={item => {
+          console.log(item)
+        }}
       />
+    )
+  }
+}
+// 分页器
+class TablePagination extends React.Component {
+  render() {
+    return (
+      <div className="block">
+        <Pagination
+          layout="total, sizes, prev, pager, next, jumper"
+          total={400}
+          pageSizes={[100, 200, 300, 400]}
+          pageSize={100}
+          currentPage={5}
+        />
+      </div>
     )
   }
 }
@@ -109,6 +171,7 @@ export default class User extends React.Component {
         <BoardList></BoardList>
         <SearchInput></SearchInput>
         <ElTable></ElTable>
+        <TablePagination></TablePagination>
       </div>
     )
   }
