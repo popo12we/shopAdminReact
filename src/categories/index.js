c0f6157bd2c373c430432ccd21384eeb3ecb728b
lujunhui <385935631@qq.com>
2020-02-15 11:14:26 +0800

    拿到table数据

--- src/categories/index.js
+++ src/categories/index.js
@@ -1,7 +1,42 @@
 import React from 'react'
 import styles from './index.module.scss'
+import { API } from '../utils'
+class CategoriesTable extends React.Component {
+  state = {
+    tableData: [],
+    pagesize: 10,
+    pagenum: 1,
+    total: 0
+  }
+  componentDidMount() {
+    this.getTableData()
+  }
+  // 拿到表格数据
+  async getTableData() {
+    let { data, meta } = await API.get('categories', {
+      params: {
+        type: 3,
+        pagenum: this.state.pagenum,
+        pagesize: this.state.pagesize
+      }
+    })
+    if (meta.status === 200) {
+      console.log(data)
+      this.setState({
+        total: data.total
+      })
+    }
+  }
+  render() {
+    return <div className="categoriesTable">CategoriesTable</div>
+  }
+}
 export default class Categories extends React.Component {
   render() {
-    return <div className={styles.categories}>Categories组件</div>
+    return (
+      <div className={styles.categories}>
+        <CategoriesTable></CategoriesTable>
+      </div>
+    )
   }
 }
