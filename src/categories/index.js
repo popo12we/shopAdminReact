9fbfc72c96917f588db247c2c772b28b5b4f5010
lujunhui <385935631@qq.com>
2020-02-15 11:36:19 +0800

    table样式

--- src/categories/index.js
+++ src/categories/index.js
@@ -1,12 +1,54 @@
 import React from 'react'
 import styles from './index.module.scss'
+import { Table, Button } from 'element-react'
 import { API } from '../utils'
 class CategoriesTable extends React.Component {
-  state = {
-    tableData: [],
-    pagesize: 10,
-    pagenum: 1,
-    total: 0
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      columns: [
+        {
+          type: 'expand',
+          expandPannel: function(data) {
+            return <div>111</div>
+          }
+        },
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          render: data => {
+            return <span>{!data.cat_deleted ? '是' : '否'}</span>
+          }
+        },
+        {
+          label: '排序',
+          render: data => {
+            return <span>{data.cat_level}</span>
+          }
+        },
+        {
+          label: '操作',
+          render: data => {
+            return (
+              <span>
+                <Button size="small">修改</Button>
+                <Button type="danger" size="small">
+                  删除
+                </Button>
+              </span>
+            )
+          }
+        }
+      ],
+      tabledata: [],
+      pagesize: 10,
+      pagenum: 1,
+      total: 0
+    }
   }
   componentDidMount() {
     this.getTableData()
@@ -23,12 +65,20 @@ class CategoriesTable extends React.Component {
     if (meta.status === 200) {
       console.log(data)
       this.setState({
-        total: data.total
+        total: data.total,
+        tabledata: data.result
       })
     }
   }
   render() {
-    return <div className="categoriesTable">CategoriesTable</div>
+    return (
+      <Table
+        style={{ width: '100%' }}
+        columns={this.state.columns}
+        data={this.state.tabledata}
+        border={false}
+      />
+    )
   }
 }
 export default class Categories extends React.Component {
