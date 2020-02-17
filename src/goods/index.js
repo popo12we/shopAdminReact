9d7fd7c9e3bd4825521f03ed5a2f24447424cae8
lujunhui <385935631@qq.com>
2020-02-17 10:54:40 +0800

    goods数据渲染

--- src/goods/index.js
+++ src/goods/index.js
@@ -1,8 +1,77 @@
 import React from 'react'
 import styles from './index.module.scss'
-import { Table } from 'element-react'
+import { API } from '../utils'
+import { Table, Button } from 'element-react'
+class GoodsTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      goodsTableData: [],
+      pagesize: 10,
+      pagenum: 1,
+      total: 0,
+      columns: [
+        {
+          type: 'index'
+        },
+        {
+          label: '商品名称',
+          prop: 'goods_name'
+        },
+        {
+          label: '商品价格',
+          prop: 'goods_price',
+          width: 90
+        },
+        {
+          label: '商品重量',
+          prop: 'goods_weight',
+          width: 90
+        }
+      ],
+      data: []
+    }
+  }
+  componentDidMount() {
+    this.getTableData()
+  }
+  async getTableData() {
+    let { data, meta } = await API.get('goods', {
+      params: {
+        pagesize: this.state.pagesize,
+        pagenum: this.state.pagenum
+      }
+    })
+    if (meta.status === 200) {
+      console.log(data)
+      this.setState({
+        total: data.total,
+        data: data.goods
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="goodstable">
+        <Button type="primary" plain>
+          添加商品
+        </Button>
+        <Table
+          style={{ width: '100%' }}
+          columns={this.state.columns}
+          data={this.state.data}
+        />
+      </div>
+    )
+  }
+}
+
 export default class Goods extends React.Component {
   render() {
-    return <div className={styles.goods}>我是goods</div>
+    return (
+      <div className={styles.goods}>
+        <GoodsTable></GoodsTable>
+      </div>
+    )
   }
 }
